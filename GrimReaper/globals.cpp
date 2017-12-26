37dce20a487013d1ddbf638a0330f8511318a4ff
Sharath Narayan <phsharathnarayan@gmail.com>
2017-12-26 12:24:00 -0600

    Initial Commit

---
+++ GrimReaper/globals.cpp
@@ -0,0 +1,10 @@
+#include "globals.h"
+
+const double Globals::PI = 3.14159;
+
+//useful for me as a dev
+bool Globals::debugging = true;
+
+//sdl related
+int Globals::ScreenWidth = 640, Globals::ScreenHeight = 352, Globals::ScreenScale = 2;
+SDL_Renderer* Globals::renderer = NULL;
---
+++ GrimReaper/globals.h
@@ -0,0 +1,22 @@
+#ifndef GLOBALS
+#define GLOBALS
+
+#include <string>
+#include <iostream>
+#include <SDL.h>
+#include "randomNumber.h"
+
+class Globals{
+public:
+	//math helpers
+	static const double PI;
+
+	//useful for me as a dev
+	static bool debugging;
+
+	//sdl related
+	static int ScreenWidth, ScreenHeight, ScreenScale;
+	static SDL_Renderer* renderer;
+};
+
+#endif
