f815196ce18b98cb79c1ec6e8c01c9916b189f28
Sharath Narayan <phsharathnarayan@gmail.com>
2018-01-05 04:50:17 -0600

    Added FrameSet and FrameSet type

---
+++ GrimReaper/frame_set.h
@@ -0,0 +1,31 @@
+#ifndef FRAMESET_H
+#define FRAMESET_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <list>
+#include <SDL.h>
+#include "frame_set_type.h"
+
+class FrameSet{
+public:
+	FrameSetType type;
+
+	FrameSet(){
+
+	}
+
+	FrameSet(FrameSetType type){
+		this->type = type;
+	}
+
+	virtual int frameSetCount() = 0;
+
+	virtual void addToSet(std::string str) = 0;
+
+	virtual void draw() = 0;
+};
+
+#endif
---
+++ GrimReaper/frame_set_type.h
@@ -0,0 +1,21 @@
+#ifndef FRAMESETTYPE_H
+#define FRAMESETTYPE_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+class FrameSetType{
+public:
+	std::string setName; 
+	int fsType; //Name or Position or Rect
+	bool isSingle;
+
+	static const int FSTYPE_NAME = 0, FSTYPE_POS = 1, FSTYPE_RECT = 2, FSTYPE_VAL = 3;
+
+	static void saveFrameSetType(std::ofstream &file, FrameSetType fsType);
+	static FrameSetType loadFrameSetType(std::ifstream &file);
+};
+
+#endif
